c897ae8d793224b562b563123a179851b3f152f9
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-07-20 04:57:58 -0400

    further updates

--- src/main/java/com/careydevelopment/autoblogger/ArticleFetcher.java
+++ src/main/java/com/careydevelopment/autoblogger/ArticleFetcher.java
@@ -1,7 +1,9 @@
 package com.careydevelopment.autoblogger;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
@@ -11,12 +13,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 
 public class ArticleFetcher {
 
@@ -56,16 +62,79 @@ public class ArticleFetcher {
 		
 		List<String> links = getLinks(url, articleCount, titleDelimiter, articleLinkStart, appendRootUrl);
 		
-		links.forEach((link) -> {
-			getArticle(link, topDelimiter, topDelimiterCount, afterContentDelimiter, tagsDelimiterTop, tagsDelimiterBottom);
-		});
+		//links.forEach((link) -> {
+			getArticle(links.get(0), topDelimiter, topDelimiterCount, afterContentDelimiter, tagsDelimiterTop, tagsDelimiterBottom);
+		//});
 	}
 	
 	
 	private void getArticle(String link, String topDelimiter, double topDelimiterCount, 
 		String afterContentDelimiter, String tagsDelimiterTop, String tagsDelimiterBottom) {
 		
+		MongoCollection<Document> articleCollection = db.getCollection("article");
+		Document exists = articleCollection.find(Filters.eq("url", link)).first();
 		
+		if (exists == null) {
+			persistArticle(link, topDelimiter, topDelimiterCount, afterContentDelimiter, tagsDelimiterTop, tagsDelimiterBottom);
+		}
+	}
+	
+	
+	private void persistArticle(String link, String topDelimiter, double topDelimiterCount, 
+		String afterContentDelimiter, String tagsDelimiterTop, String tagsDelimiterBottom) {
+		
+		String text = getTextFromLink(link);
+		String blurb = getBlurb(text, topDelimiter, topDelimiterCount, afterContentDelimiter);
+	}	
+	
+	
+	private String getBlurb(String text, String topDelimiter, double topDelimiterCount, String afterContentDelimiter) {
+		int start = -1;
+
+		for (double i=0; i <= topDelimiterCount;i++) {
+			start = text.indexOf(topDelimiter, start + 1);
+		}
+		
+		if (start > -1) {
+			int end = text.indexOf(afterContentDelimiter, start + 1);
+			if (end > -1) {
+				String rawContent = text.substring(start + topDelimiter.length(), end);
+				rawContent = rawContent.trim();
+				try {
+					try (BufferedReader reader = new BufferedReader(new StringReader(rawContent))) {
+						try (Stream<String> stream = reader.lines()) {
+							stream.forEach(line -> {
+								System.err.println(line);
+							});
+						}
+					}
+				} catch (Exception e) {
+					LOGGER.error("Problem getting blurb from text " + text.substring(0, 40), e);
+				}
+			}
+		}
+		
+		return "";
+	}
+	
+	
+	private String getTextFromLink(String url) {
+	    BodyContentHandler handler = new BodyContentHandler();
+
+		try {
+			URLConnection conn = new URL(url).openConnection();
+			conn.addRequestProperty("User-Agent", "Mozilla/4.76");
+								    
+		    AutoDetectParser parser = new AutoDetectParser();
+		    Metadata metadata = new Metadata();
+		    try (InputStream stream = conn.getInputStream()) {
+		        parser.parse(stream,handler,metadata);
+		    }
+		} catch (Exception e) {
+			LOGGER.error("Problem reading URL " + url, e);
+		}
+		
+		return handler.toString();
 	}
 	
 	
